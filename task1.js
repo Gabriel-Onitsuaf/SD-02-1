ee0ef5a818544ab72a7ca54a4d211d8afb96171d
Gabriel Flores <gabrielonitsuaf@gmail.com>
2025-12-15 14:56:27 -0600

    Entrega de laboratorio codificación de bucles

--- task1.js
+++ task1.js
@@ -1,3 +1,6 @@
-// Refer to Task 1 in your Instructions to complete this task
-const prompt = require("prompt-sync")();
-const name = prompt("Ingresa tu nombre");
+//Creando programa en JavaScript que imprima los números del 1 al 105.
+
+for (let i = 1; i <= 105; i++) {
+    if (i % 3 === 0 ) 
+        console.log(i);
+    }
--- task3.js
+++ task3.js
@@ -1,5 +1,13 @@
-// Refer to Task 3 in your Instructions to complete this task
+// Remplazar cada número divisible por 5 con Buzz, y cada número que cumpla ambas condiciones con FizzBuzz
 
-for (let i = 0; i < 1; i++) {
-    console.log("This is Task Three!");
-  };
+for (let i = 1; i <= 105; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  }else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
+}
